98c7dccf43a2924164700374038533a3c0ff43fa
cristinarv <c.delriove@alumnos.urjc.es>
2018-03-12 02:48:39 +0100

    Ya estaría hecho el metodo POST

--- practica1.py
+++ practica1.py
@@ -11,15 +11,11 @@ class webApp (webapp.webApp):
     def process(self, parsedRequest):
         method, recurso, information = parsedRequest 
         self.contador = len(self.completlyUrls)
-        if method == method == "GET":
+        if method =="GET":
             if recurso =="/":
                 returnCode = "HTTP/1.1 200 OK"
                 htmlAnswer = ("<html><body>" + "<form method = 'POST'>" + "La URL que se quiere acortar es: " + "<input type='text' name='url'><br>" +
                               "<input type='submit' value='Acorta la url'></form>" + str(self.tightUrls) + "</body></html>")
-            elif recurso == "/favicon.ico":
-                 return("404 Not Found",
-                        "<html><body><h1>Recurso no encontrado" +
-                        "</h1></body></html>")
             else:
                 recurso = int(recurso[1:])
                 if recurso in self.tightUrls:
@@ -29,10 +25,35 @@ class webApp (webapp.webApp):
                 else:
                     returnCode = "HTTP/1.1 404 Not Found"
                     htmlAnswer = ("<html><body>" +
-                                  "recurso Not FoundR</body></html>")
+                                  "recurso Not FoundR</body></html>"
+        if metodo =="POST":
+            if body == "":  # Es un formulario vacio
+                return("404 Not Found",
+                       "<html><body><h1>Formulario vacio, meta una url" +
+                       "</h1></body></html>")
+            else:
+                if (body[0:13] == "http%3A%2F%2F" or
+                        body[0:14] == "https%3A%2F%2F"):
+                    body = unquote(body)
+                else:
+                    body = "http://" + body  # Ponemos una cabecera
+                if body in self.urls_orig:
+                    url_corta = ("http://localhost:1234/" +
+                                 str(self.urls_orig[body]))
+                    return("200 Ok", "<html><body>La url_corta es:6666666666 " +
+                           "<a href=" + url_corta + ">" + url_corta +
+                           "</a>" + "</body></html>")
+                else:
+                    self.urls_orig[body] = self.urlsCont
+                    enlace = "http://localhost:1234/" + str(self.urlsCont)
+                    self.urls_cortas[self.urlsCont] = body
+                    self.urlsCont = self.urlsCont + 1
+                    return("HTTP/1.1 200 Ok", "<html><body>" +
+                           "<a href=" + enlace + ">" +
+                           enlace + "</a>" + "</body></html>")
         else:
             returnCode = "HTTP/1.1 405 Method Not Allowed"
             htmlAnswer = "Method Not Allowed"
-        return (returnCode, htmlAnswer)
+        
 if __name__ == "__main__":
     testWebApp = webApp("localhost", 1234)
